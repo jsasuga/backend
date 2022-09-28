953d2f446015dee7ba7c39bb8a189a78bef4c472
Javier Sasuga <jsasuga@gmail.com>
2022-09-27 19:36:20 -0600

    basic auth

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
-import 'dotenv/config';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
-const port = process.env.PORT;
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(port);
+    const app: NestExpressApplication = await NestFactory.create(AppModule);
+    const config: ConfigService = app.get(ConfigService);
+    const port: number = config.get<number>('PORT');
+
+    app.useGlobalPipes(
+        new ValidationPipe({ whitelist: true, transform: true }),
+    );
+
+    await app.listen(port, () => {
+        console.log('[WEB]', `http://localhost:${port}`);
+    });
 }
 
 bootstrap();
