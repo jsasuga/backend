dfa8fff0de28aec11e4a95ef873371289a00ec24
Javier Sasuga <jsasuga@gmail.com>
2022-11-08 10:10:50 -0600

    victimid to user

--- src/api/user/user.entity.ts
+++ src/api/user/user.entity.ts
@@ -1,5 +1,5 @@
 import { Exclude } from 'class-transformer';
-import { BaseEntity, Column, DeleteDateColumn, Entity, ManyToOne, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity, Column, DeleteDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
 import { Role } from '../role/role.entity';
 import { Provider } from '../provider/provider.entity';
@@ -39,7 +39,7 @@ export class User extends BaseEntity {
   @ApiProperty()
   public provider: Provider;
 
-  @OneToOne(() => Victim)
+  @ManyToOne(() => Victim)
   @ApiProperty()
   public victim: Victim;
 
