d78b7112e7d6ad557a5b080782ae496380496692
Javier Gomez <rjgomezs95+123@gmail.com>
2023-02-21 19:39:19 -0600

    script changes

--- scripts/0_make_scripts_executable.sh
+++ scripts/0_make_scripts_executable.sh
@@ -4,5 +4,6 @@ chmod +x ./scripts/1_initial_setup.sh
 chmod +x ./scripts/2_database_setup_RUN_MANUALLY.sh
 chmod +x ./scripts/3_switch_env_files.sh
 chmod +x ./scripts/4_run_migrations.sh
+chmod +x ./scripts/5_create_admin_user.sh
 chmod +x ./scripts/kill_process_on_port.sh
 chmod +x ./scripts/serve_with_output.sh
