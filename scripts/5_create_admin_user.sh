35e52ab5865e1f35825b0af40532ec00bc04b290
Javier Gomez <rjgomezs95+123@gmail.com>
2023-02-21 19:55:02 -0600

    curl changes

--- scripts/5_create_admin_user.sh
+++ scripts/5_create_admin_user.sh
@@ -6,8 +6,11 @@ curl --request POST \
   --data  '{ "email": "admin@admin.com", "password": "password", "name": "Admin", "userCode": "ADMIN_CODE", "roleId": 3, "providerId": 1 }' \
   --url http://localhost:1938/auth/register
 
+echo "----------"
+echo "-----TRY TO LOGIN WITH CREDENTIALS-----"
 # test login with admin user
 curl --request POST \
   --header 'Content-Type: application/json' \
   --data  '{ "email": "admin@admin.com", "password": "password" }' \
   --url http://localhost:1938/auth/login
+echo "----------"
--- scripts/serve_with_output.sh
+++ scripts/serve_with_output.sh
@@ -2,3 +2,5 @@
 
 cd ~/backend/
 yarn run start:prodbg &> serve_prod.output
+sleep 5
+sudo netstat -nlp | grep :1938
